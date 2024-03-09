4c703a25ca63f4e015058c183cb6eca7cbd78e33
sophwa <sophwang024@gmail.com>
2024-03-09 18:31:24 -0500

    Created admin model

--- frontend/util/models.ts
+++ frontend/util/models.ts
@@ -1,7 +1,12 @@
 import { Document } from 'mongoose';
 // Please add models here
 
-interface IUser extends Document{
-    uid: string;
-    name: string;
-  }
+interface IUser extends Document {
+  uid: string;
+  name: string;
+}
+
+interface IAdmin extends IUser {
+  email: string;
+  privilegeMode: string;
+}
