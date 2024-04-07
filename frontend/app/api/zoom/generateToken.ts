556eaa82dd7f5c1a7d202e39f9e614924cdd5d18
mohammad0612 <mai212228@gmail.com>
2024-04-07 18:10:15 -0400

    attempting to debug

--- frontend/app/api/zoom/generateToken.ts
+++ frontend/app/api/zoom/generateToken.ts
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import { NextResponse } from "next/server";
 import { NextApiRequest, NextApiResponse } from 'next/types';
+import { PrismaClient} from "@prisma/client";
+import qs from 'query-string';
+
+const prisma = new PrismaClient();
 
 // function to generate zoom access token 
-const  generateZoomToken = async () => {
+const generateZoomToken = async () => {
   try {
     const clientId = process.env.NEXT_PUBLIC_ZOOM1_CLIENT_ID;
     const clientSecret = process.env.NEXT_PUBLIC_ZOOM1_CLIENT_SECRET;
     const accountId = process.env.NEXT_PUBLIC_ZOOM1_ACCOUNT_ID;
-    console.log("hi2");
   
     // check if the environment variables are set
     if (!clientId || !clientSecret || !accountId) {
@@ -16,12 +19,13 @@ const  generateZoomToken = async () => {
     }
 
     // make a post req to zoom api 
-    const request = await axios.post('https://zoom.us/oauth/token', {
+    const request = await axios.post('https://zoom.us/oauth/token',  qs.stringify({
       grant_type: 'account_credentials',
       account_id: accountId,
-    }, {
+    }), {
         headers: {
-        'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
         }
       }
     );
@@ -36,18 +40,22 @@ const  generateZoomToken = async () => {
 
 // function to export access token 
 const getZoomToken = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const zoomToken = await generateZoomToken();
+  if (req.method === 'GET') {
+    try {
+      const zoomToken = await generateZoomToken();
 
-    if (!zoomToken) {
-      return NextResponse.json({ error: 'Error generating Zoom token' }, { status: 500 });
+      if (!zoomToken) {
+        return NextResponse.json({ error: 'Error generating Zoom token' }, { status: 500 });
+      }
+
+      return NextResponse.json({ access_token: zoomToken }, { status: 200 });
+    } catch (error) {
+      console.error('Error getting Zoom token:', error);
+      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
-
-    return NextResponse.json({ access_token: zoomToken }, { status: 200 });
-  } catch (error) {
-    console.error('Error getting Zoom token:', error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  } else {
+    return NextResponse.json({ error: 'Method Not Allowed' }, { status: 405 });
   }
 }
 
-export { getZoomToken as GET}
+export { getZoomToken as GET };
