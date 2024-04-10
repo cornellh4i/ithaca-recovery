942640a2fb3173920f712a4ea5b0debf066ef3e5
Srija Ghosh <sg2293@cornell.edu>
2024-04-10 18:59:02 -0400

    added a retrieve meeting, update meeting, and delete meeting functionality

---
+++ frontend/app/api/retrieve/meeting/route.ts
@@ -0,0 +1,29 @@
+import { IMeeting } from "../../../../util/models";
+import { PrismaClient } from '@prisma/client';
+
+
+const prisma = new PrismaClient();
+
+
+export const retrieveMeetings = async (request: Request) => {
+  try {
+    const meetings = await prisma.meeting.findMany();
+    const typedMeetings: IMeeting[] = meetings.map(meeting => ({ ...meeting }))
+    return new Response(JSON.stringify(typedMeetings), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error("Error retrieving meetings: ", error);
+    return new Response(JSON.stringify({ error: "Error retrieving meetings" }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
+
+export { retrieveMeetings as GET }
