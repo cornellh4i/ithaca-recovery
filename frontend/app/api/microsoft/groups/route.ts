9e5f8c02ef7e9a520886c5262b32c6201fac9c0d
Joseph Ugarte <jeugarte14@gmail.com>
2024-09-12 18:06:40 +0900

    Revert commenting test

--- frontend/app/api/microsoft/groups/route.ts
+++ frontend/app/api/microsoft/groups/route.ts
@@ -1,46 +1,46 @@
-// // export const dynamic = 'force-dynamic';
-// import { authProvider } from "../../../../services/auth";
-// import getAccessToken from '../AccessToken';
+// export const dynamic = 'force-dynamic';
+import { authProvider } from "../../../../services/auth";
+import getAccessToken from '../AccessToken';
 
-// const getGroups = async (req: Request) => {
-//   try {
-//     const accessToken = await getAccessToken();
-//     if (accessToken === null) {
-//       console.log("Unable to retrieve access token, please try again.")
-//       return;
-//     }
+const getGroups = async (req: Request) => {
+  try {
+    const accessToken = await getAccessToken();
+    if (accessToken === null) {
+      console.log("Unable to retrieve access token, please try again.")
+      return;
+    }
 
-//     const endpoint = `${process.env.NEXT_PUBLIC_GRAPH_API_ENDPOINT}/groups`;
-//     const headers = {
-//       'Authorization': `Bearer ${accessToken}`,
-//       'Content-Type': 'application/json'
-//     };
+    const endpoint = `${process.env.NEXT_PUBLIC_GRAPH_API_ENDPOINT}/groups`;
+    const headers = {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json'
+    };
 
-//     const response = await fetch(endpoint, {
-//       method: 'GET',
-//       headers: headers
-//     });
+    const response = await fetch(endpoint, {
+      method: 'GET',
+      headers: headers
+    });
 
-//     if (!response.ok) {
-//       throw new Error('Error fetching groups');
-//     }
+    if (!response.ok) {
+      throw new Error('Error fetching groups');
+    }
 
-//     const data = await response.json();
-//     return new Response(JSON.stringify(data), {
-//       status: 200,
-//       headers: { 'Content-Type': 'application/json' },
-//     });
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-//   } catch (error) {
-//     console.error('Error fetching groups:', error);
-//     return new Response(
-//       JSON.stringify({ error: 'Error fetching groups' }),
-//       {
-//         status: 500,
-//         headers: { 'Content-Type': 'application/json' },
-//       }
-//     );
-//   }
-// }
+  } catch (error) {
+    console.error('Error fetching groups:', error);
+    return new Response(
+      JSON.stringify({ error: 'Error fetching groups' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+}
 
-// export { getGroups as GET }
+export { getGroups as GET }
