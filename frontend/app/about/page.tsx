365f0d2a9a353cb383f91f2582c69fdae5d46d86
Phoebe Qian <pqian7@gmail.com>
2024-09-22 08:42:26 -0400

    A cat can spend five or more hours a day grooming himself

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,12 +1,25 @@
+"use client";
 import React from "react";
 import Button from "../components/button"; 
 
 /** An About page */
+
+
+const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    console.log(response.json());
+  };
+  
+  const handleClick = () => {
+    getCatFact();
+  };
+
 const About = () => {
   return (
     <>
       <Button text="hello" />
       Hello there
+      <button onClick={handleClick}>cat fact</button>
     </>
   );
 };
