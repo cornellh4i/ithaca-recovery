420872910f29f529b1870d9c9dd096571e35a70a
Alisha <av523@cornell.edu>
2024-09-29 17:16:43 -0400

    An adult lion's roar can be heard up to five miles (eight kilometers) away.

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Button from "../components/button";
 
@@ -7,8 +9,19 @@ const About = () => {
     <>
       <Button text="hello" />
       Hello there
+
+      <button onClick={handleClick}>cat fact</button>
     </>
   );
 };
 
 export default About;
+
+const getCatFact = async () => {
+  const response = await fetch("https://catfact.ninja/fact");
+  console.log(response.json());
+};
+
+const handleClick = () => {
+  getCatFact();
+};
