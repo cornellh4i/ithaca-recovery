330effb830fd8733739166c3857b02bd3951c2ff
brandon-lerit <148146640+brandon-lerit@users.noreply.github.com>
2024-09-28 23:17:13 -0400

    "Cats should not be fed tuna exclusively, as it lacks taurine, an essential nutrient required for good feline health.  Make sure you have the proper Pet supplies to keep your cat happy and healthy."

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,11 +1,22 @@
+"use client";
 import React from "react";
 import Button from "../components/button"; 
 
 /** An About page */
 const About = () => {
+
+    const getCatFact = async () => {
+        const response = await fetch("https://catfact.ninja/fact");
+        console.log(response.json());
+      };
+      
+      const handleClick = () => {
+        getCatFact();
+      };
+
   return (
     <>
-      <Button text="hello" />
+      <button onClick={handleClick}>cat fact</button>
       Hello there
     </>
   );
