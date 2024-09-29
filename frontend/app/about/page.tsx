a48cb778c18321ad1bb35259c01a8fe8ac4aa706
sophwa <sophwang024@gmail.com>
2024-09-29 04:59:53 -0400

    Approximately 24 cat skins can make a coat.

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,12 +1,22 @@
+"use client";
 import React from "react";
 import Button from "../components/button";
 
 /** An About page */
+const getCatFact = async () => {
+  const response = await fetch("https://catfact.ninja/fact");
+  console.log(response.json());
+};
+
+const handleClick = () => {
+  getCatFact();
+};
 const About = () => {
   return (
     <>
       <Button />
       Hello there
+      <button onClick={handleClick}>cat fact</button>
     </>
   );
 };
