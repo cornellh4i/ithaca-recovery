4e10ecff70cf13b8d908f3255f6be2d8a27efe62
Sophie Strausberg <sophie@dhcp-vl2041-28220.eduroam.cornell.edu>
2024-10-04 16:24:13 -0400

    A cat called Dusty has the known record for the most kittens. She had more than 420 kittens in her lifetime.

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,10 +1,20 @@
+"use client";
 import React from "react";
 import Button from "../components/button"; 
+
+const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    console.log(response.json());
+};
+  
+const handleClick = () => {
+  getCatFact();
+};
 
 /** An About page */
 const About = () => {
   return (
-    <Button text="hello" />
+    <button onClick={handleClick}>cat fact</button>
   );
 };
 
