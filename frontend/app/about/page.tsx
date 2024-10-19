b15a04fa531dc24d14d069a486e05b6cfdc0731f
Leon Huang <lyh7@cornell.edu>
2024-10-19 02:12:03 -0400

    "A cat's brain is more similar to a man's brain than that of a dog.

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Button from "../components/button"; 
 
@@ -7,8 +8,18 @@ const About = () => {
     <>
       <Button text="hello" />
       Hello there
+      <button onClick={handleClick}>cat fact</button>
     </>
   );
 };
+
+const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    console.log(response.json());
+  };
+  
+  const handleClick = () => {
+    getCatFact();
+  };
 
 export default About;
