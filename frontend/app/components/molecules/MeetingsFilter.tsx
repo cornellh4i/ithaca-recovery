010d86668505a70f9c8eb9c2626be9c9bc2f43fb
brandon-lerit <148146640+brandon-lerit@users.noreply.github.com>
2024-10-19 12:31:00 -0400

    Finished and Tested Meeting Filter Component

--- frontend/app/components/molecules/MeetingsFilter.tsx
+++ frontend/app/components/molecules/MeetingsFilter.tsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
 import LabeledCheckbox from '../atoms/checkbox/index';
+import styles from '../../../styles/components/molecules/MeetingsFilter.module.scss';
 
 const MeetingsFilter: React.FC = () => {
     // State to track the checked status of each filter
     const [filters, setFilters] = useState({
-        SerenityRoom: true,
-        SeedsOfHope: true,
-        UnityRoom: true,
-        RoomForImprovement: true,
-        SmallButPowerfulRight: true,
-        SmallButPowerfulLeft: true,
+        SerenityRoom: false,
+        SeedsOfHope: false,
+        UnityRoom: false,
+        RoomForImprovement: false,
+        SmallButPowerfulRight: false,
+        SmallButPowerfulLeft: false,
         ZoomAccount1: false,
         ZoomAccount2: false,
         ZoomAccount3: false,
         ZoomAccount4: false,
-        AA: true,
-        AlAnon: true,
-        Other: true,
-        InPerson: true,
-        Hybrid: true
+        AA: false,
+        AlAnon: false,
+        Other: false,
+        InPerson: false,
+        Hybrid: false
     });
 
     // Handler to update the filter state
@@ -29,100 +30,130 @@ const MeetingsFilter: React.FC = () => {
     return (
         <div>
             <h3>Location</h3>
-            <LabeledCheckbox
-                label="Serenity Room"
-                checked={filters.SerenityRoom}
-                onChange={handleCheckboxChange('SerenityRoom')}
-                color="#B6E5A3"
-            />
-            <LabeledCheckbox
-                label="Seeds of Hope"
-                checked={filters.SeedsOfHope}
-                onChange={handleCheckboxChange('SeedsOfHope')}
-                color="#FFEC84"
-            />
-            <LabeledCheckbox
-                label="Unity Room"
-                checked={filters.UnityRoom}
-                onChange={handleCheckboxChange('UnityRoom')}
-                color="#A3DFFF"
-            />
-            <LabeledCheckbox
-                label="Room for Improvement"
-                checked={filters.RoomForImprovement}
-                onChange={handleCheckboxChange('RoomForImprovement')}
-                color="#FFB383"
-            />
-            <LabeledCheckbox
-                label="Small but Powerful - Right"
-                checked={filters.SmallButPowerfulRight}
-                onChange={handleCheckboxChange('SmallButPowerfulRight')}
-                color="#D3A3FF"
-            />
-            <LabeledCheckbox
-                label="Small but Powerful - Left"
-                checked={filters.SmallButPowerfulLeft}
-                onChange={handleCheckboxChange('SmallButPowerfulLeft')}
-                color="#FFB3B3"
-            />
-            <LabeledCheckbox
-                label="Zoom Account 1"
-                checked={filters.ZoomAccount1}
-                onChange={handleCheckboxChange('ZoomAccount1')}
-                color="#D3D3D3"
-            />
-            <LabeledCheckbox
-                label="Zoom Account 2"
-                checked={filters.ZoomAccount2}
-                onChange={handleCheckboxChange('ZoomAccount2')}
-                color="#D3D3D3"
-            />
-            <LabeledCheckbox
-                label="Zoom Account 3"
-                checked={filters.ZoomAccount3}
-                onChange={handleCheckboxChange('ZoomAccount3')}
-                color="#D3D3D3"
-            />
-            <LabeledCheckbox
-                label="Zoom Account 4"
-                checked={filters.ZoomAccount4}
-                onChange={handleCheckboxChange('ZoomAccount4')}
-                color="#D3D3D3"
-            />
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Serenity Room"
+                    checked={filters.SerenityRoom}
+                    onChange={handleCheckboxChange('SerenityRoom')}
+                    color="#B3EA75"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Seeds of Hope"
+                    checked={filters.SeedsOfHope}
+                    onChange={handleCheckboxChange('SeedsOfHope')}
+                    color="#F7E57B"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Unity Room"
+                    checked={filters.UnityRoom}
+                    onChange={handleCheckboxChange('UnityRoom')}
+                    color="#96DBFE"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Room for Improvement"
+                    checked={filters.RoomForImprovement}
+                    onChange={handleCheckboxChange('RoomForImprovement')}
+                    color="#FFAE73"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Small but Powerful - Right"
+                    checked={filters.SmallButPowerfulRight}
+                    onChange={handleCheckboxChange('SmallButPowerfulRight')}
+                    color="#D2AFFF"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Small but Powerful - Left"
+                    checked={filters.SmallButPowerfulLeft}
+                    onChange={handleCheckboxChange('SmallButPowerfulLeft')}
+                    color="#FFA3C2"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Zoom Account 1"
+                    checked={filters.ZoomAccount1}
+                    onChange={handleCheckboxChange('ZoomAccount1')}
+                    color="#CECECE"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Zoom Account 2"
+                    checked={filters.ZoomAccount2}
+                    onChange={handleCheckboxChange('ZoomAccount2')}
+                    color="#CECECE"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Zoom Account 3"
+                    checked={filters.ZoomAccount3}
+                    onChange={handleCheckboxChange('ZoomAccount3')}
+                    color="#CECECE"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Zoom Account 4"
+                    checked={filters.ZoomAccount4}
+                    onChange={handleCheckboxChange('ZoomAccount4')}
+                    color="#CECECE"
+                />
+            </div>
 
             <h3>Calendar</h3>
-            <LabeledCheckbox
-                label="AA"
-                checked={filters.AA}
-                onChange={handleCheckboxChange('AA')}
-                color="#B90076"
-            />
-            <LabeledCheckbox
-                label="Al-Anon"
-                checked={filters.AlAnon}
-                onChange={handleCheckboxChange('AlAnon')}
-                color="#B90076"
-            />
-            <LabeledCheckbox
-                label="Other"
-                checked={filters.Other}
-                onChange={handleCheckboxChange('Other')}
-                color="#B90076"
-            />
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="AA"
+                    checked={filters.AA}
+                    onChange={handleCheckboxChange('AA')}
+                    color="#CC3366"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Al-Anon"
+                    checked={filters.AlAnon}
+                    onChange={handleCheckboxChange('AlAnon')}
+                    color="#CC3366"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="Other"
+                    checked={filters.Other}
+                    onChange={handleCheckboxChange('Other')}
+                    color="#CC3366"
+                />
+            </div>
 
             <h3>Mode</h3>
-            <LabeledCheckbox
-                label="In Person"
-                checked={filters.InPerson}
-                onChange={handleCheckboxChange('InPerson')}
-                color="#E60023"
-            />
-            <LabeledCheckbox
-                label="Hybrid"
-                checked={filters.Hybrid}
-                onChange={handleCheckboxChange('Hybrid')}
-                color="#E60023"
-            />
+            <div className={styles.checkbox}>
+                <LabeledCheckbox
+                    label="In Person"
+                    checked={filters.InPerson}
+                    onChange={handleCheckboxChange('InPerson')}
+                    color="#CC3366"
+                />
+            </div>
+            <div className={styles.checkbox}>
+                <LabeledCheckbox 
+                    label="Hybrid"
+                    checked={filters.Hybrid}
+                    onChange={handleCheckboxChange('Hybrid')}
+                    color="#CC3366"
+                />
+            </div>
         </div>
     );
 };
