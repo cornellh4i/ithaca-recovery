c6e5a17a5ffc413cb9405a90320090d3732aefc9
Leon Huang <lyh7@cornell.edu>
2024-11-02 14:11:38 -0400

    Fixed issue with shifting box

--- frontend/app/components/atoms/MiniCalendar/index.tsx
+++ frontend/app/components/atoms/MiniCalendar/index.tsx
@@ -5,32 +5,37 @@ import styles from "../../../../styles/components/atoms/MiniCalendar.module.scss
 
 const MiniCalendar: React.FC = () => {
     const [selectedDate, setSelectedDate] = useState<Date | undefined>();
+    const [month, setMonth] = useState(new Date());
 
     return (
-        <div style={{ padding: '12px', backgroundColor: '#FFF', borderRadius: '4px', width: 'fit-content' }}>
-            {}
-            <h2 style={{ textAlign: 'center', fontSize: '1em', fontWeight: 'bold' }}>Calendar</h2>
+        <div className={styles.container}>
+            <h2 className={styles.header}>Calendar</h2>
 
-            {}
-            <DayPicker
-                mode="single"
-                selected={selectedDate}
-                onSelect={setSelectedDate}
-                showOutsideDays
-                classNames={{
-                    month: styles.rdpMonth,   
-                    day: styles.day,          
-                    outside: styles.dayOutside,
-                    selected: styles.selectedDay, 
-                    weekday: styles.weekday,
-                    today: styles.today
-                }}
-                formatters={{
-                    formatWeekdayName: (date) => {
-                        return date.toLocaleDateString('en-US', { weekday: 'short' })[0];
-                    },
-                }}
-            />
+            <div className={styles.calendarContainer}>
+                
+                <DayPicker
+                    mode="single"
+                    selected={selectedDate}
+                    onSelect={setSelectedDate}
+                    month={month}
+                    onMonthChange={setMonth}
+                    showOutsideDays
+                    classNames={{
+                        month: styles.rdpMonth,
+                        day: styles.day,
+                        outside: styles.dayOutside,
+                        selected: styles.selectedDay,
+                        weekday: styles.weekday,
+                        today: styles.today
+                    }}
+                    formatters={{
+                        formatWeekdayName: (date) => {
+                            return date.toLocaleDateString('en-US', { weekday: 'short' })[0];
+                        },
+                    }}
+                />
+
+            </div>
         </div>
     );
 };
