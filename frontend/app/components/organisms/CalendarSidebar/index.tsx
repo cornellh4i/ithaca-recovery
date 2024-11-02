8af33ab96762720e1069da930cbe477b1eb6603a
sophwa <sophwang024@gmail.com>
2024-11-02 13:13:14 -0400

    Implemented calendar sidebar frontend with button functionality

---
+++ frontend/app/components/organisms/CalendarSidebar/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import TextButton from '../../atoms/textbutton';
+import TextField from '../../atoms/TextField';
+import DatePicker from '../../atoms/DatePicker';
+import TimePicker from '../../atoms/TimePicker';
+import RadioGroup from '../../atoms/RadioGroup';
+import MeetingsFilter from '../../molecules/MeetingsFilter';
+import NewMeetingSidebar from '../NewMeeting';
+import AddIcon from '@mui/icons-material/Add';
+import styles from '../../../../styles/components/organisms/CalendarSidebar.module.scss';
+
+const CalendarSidebar: React.FC = () => {
+  const [isNewMeetingOpen, setIsNewMeetingOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(''); // Default to an empty string
+
+  const handleOpenNewMeeting = () => {
+    setIsNewMeetingOpen(true);
+  };
+
+  const handleCloseNewMeeting = () => {
+    setIsNewMeetingOpen(false);
+  };
+
+  return (
+    <div>
+      {isNewMeetingOpen ? (
+        <NewMeetingSidebar //Placeholder
+          TextField={<TextField label="Meeting Title" onChange={() => { }} />}
+          DatePicker={<DatePicker label="Select Date" />}
+          TimePicker={<TimePicker label="Select Time" />}
+          RadioGroup={
+            <RadioGroup
+              label="Select Option"
+              options={["", "", ""]}
+              selectedOption={selectedOption}
+              onChange={setSelectedOption}
+              name="preferences"
+              disabledOptions={[]}
+            />
+          }
+        />
+      ) : (
+        <>
+          <TextButton label="New Meeting" onClick={handleOpenNewMeeting} icon={<AddIcon />} />
+          <div className={styles.meetingsFilter}>
+            <MeetingsFilter />
+          </div>
+        </>
+      )}
+      {/* Close button, if needed */}
+      {isNewMeetingOpen && (
+        <button onClick={handleCloseNewMeeting}>Close</button> // Simple close button
+      )}
+    </div>
+  );
+};
+
+export default CalendarSidebar;
+
---
+++ frontend/app/components/organisms/NewMeeting/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from '../../../../styles/organisms.NewMeeting.module.scss';
+
+interface NewMeetingSidebarProps {
+  TextField: React.ReactElement;
+  DatePicker: React.ReactElement;
+  TimePicker: React.ReactElement;
+  RadioGroup: React.ReactElement;
+}
+
+const NewMeetingSidebar: React.FC<NewMeetingSidebarProps> = ({ TextField, DatePicker, TimePicker, RadioGroup }) => {
+  return (
+    <div className={styles.newMeetingSidebar}>
+      <div className={styles.dummyComponent}>
+        {TextField}
+      </div>
+      <div className={styles.dummyComponent}>
+        {DatePicker}
+      </div>
+      <div className={styles.dummyComponent}>
+        {TimePicker}
+      </div>
+      <div className={styles.dummyComponent}>
+        {RadioGroup}
+      </div>
+      <button className={styles.createMeetingButton}>Create Meeting</button>
+    </div>
+  );
+};
+
+export default NewMeetingSidebar;
