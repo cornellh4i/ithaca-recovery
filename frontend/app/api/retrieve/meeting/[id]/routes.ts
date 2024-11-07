666a226a042f60269986bf3ec13d8cdefb51618c
Alisha <av523@cornell.edu>
2024-11-07 18:52:52 -0500

    Fixed tiny error

--- frontend/app/api/retrieve/meeting/[id]/routes.ts
+++ frontend/app/api/retrieve/meeting/[id]/routes.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { NextApiRequest } from "next";
-import { IMeeting } from "../../../../util/models";
+import { IMeeting } from "../../../../../util/models";
 import { NextRequest } from "next/server";
 
 const prisma = new PrismaClient();
