938cb6ffefe369dd5b35319e462d20b5fb178621
dvala041 <86671843+dvala041@users.noreply.github.com>
2025-02-25 16:35:56 -0500

    Kittens remain with their mother till the age of 9 weeks

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,11 +1,23 @@
+"use client";
 import React from "react";
 import Button from "../components/button";
 
 /** An About page */
 const About = () => {
+  
+  const getCatFact = async() => {
+    const response = await fetch("https://catfact.ninja/fact");
+    console.log(response.json());
+  };
+  
+  const handleClick = () => {
+    getCatFact();
+  }
+
   return (
     <>
       <Button text="Hello There" hi="hi"/>
+      <button onClick={handleClick}>cat fact</button>
     </>
     
   )
