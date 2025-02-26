3c8da54d0d7292a59080163723d2a9f38128078c
Sheki Okwayo <lso24@cornell.edu>
2025-02-25 22:32:30 -0500

    Complete components onboarding

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "../components/button"; 
+
+/** An About page */
+const About = () => {
+  return (
+    <>
+      <Button />
+      Hello there
+    </>
+  );
+};
+
+export default About;
