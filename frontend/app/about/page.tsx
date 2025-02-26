1978bc3998d08c1a26c10bbba2b442719c846da8
Sheki Okwayo <lso24@cornell.edu>
2025-02-25 22:49:04 -0500

    A cat cannot see directly under its nose

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Button from "../components/button"; 
 
@@ -7,8 +8,18 @@ const About = () => {
     <>
       <Button text="hello" />
       Hello there
+      <button onClick={handleClick}>cat fact</button>
     </>
   );
 };
+const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    const data = await response.json(); 
+    console.log("Cat Fact:", data.fact); 
+  };
+  
+const handleClick = () => {
+    getCatFact();
+  };
 
 export default About;
