aa99042b87b629b41fe52aa80392b676de2dfa19
Tanya Aravind <ta374@cornell.edu>
2025-03-02 19:05:34 -0500

    A cat's brain is more similar to a man's brain than that of a dog.

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Button from "../components/button"; 
+
 
 /** An About page */
 const About = () => {
@@ -7,8 +9,18 @@ const About = () => {
     <>
       <Button text="hello" />
       Hello there
+      <button onClick={handleClick}>cat fact</button>
     </>
   );
 };
+
+const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    console.log(response.json());
+  };
+  
+  const handleClick = () => {
+    getCatFact();
+  };
 
 export default About;
