450ebb85426234a0e4159e27523ff63fa85f9d23
Samantha Cruz <samantha.jaycruz@gmail.com>
2025-03-02 21:53:07 -0500

    When your cats rubs up against you, she is actually marking you as 'hers' with her scent. If your cat pushes his face against your head, it is a sign of acceptance and affection.

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,13 +1,27 @@
+"use client";
 import React from "react";
 import Button from "../components/button"; 
+
+const getCatFact = async () => {
+  const response = await fetch("https://catfact.ninja/fact");
+  console.log(response.json());
+};
+
+const handleClick = () => {
+  getCatFact();
+};
 
 /** An About page */
 const About = () => {
   return (
     <>
       <Button text = "hello" />
+      Hello there
+      <button onClick={handleClick}>cat fact</button>
     </>
   );
 };
+
+
 
 export default About;
