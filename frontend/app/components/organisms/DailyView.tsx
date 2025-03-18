2c1445a2fc7c0c5bd8cc54481cbbbb7f9c353c15
Tuni Le <ltn090403@gmail.com>
2025-03-17 21:47:28 -0400

    Refactor DailyView

--- frontend/app/components/organisms/DailyView.tsx
+++ frontend/app/components/organisms/DailyView.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from '../../../../styles/organisms/DailyView.module.scss';
+import styles from '../../../styles/organisms/DailyView.module.scss';
 import BoxText from '../atoms/BoxText';
 import DailyViewRow from "../molecules/DailyViewRow";
 
