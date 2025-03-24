ca64378a46886cf40f9714a61db7d3bcb7900a8d
Tanya Aravind <ta374@cornell.edu>
2025-03-23 21:24:26 -0400

    Completed error messaging for atoms/TextField
    - edited styling to add error message styling
    - edited to handle error after blurring
    - followed formatting for Datepicker

--- frontend/app/components/atoms/TextField/index.tsx
+++ frontend/app/components/atoms/TextField/index.tsx
@@ -17,15 +17,41 @@ const TextField: React.FC<TextFieldProps> = ({
   label,
   ...props
 }) => {
+
+  const [internalValue, setInternalValue] = useState<string>(value);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [inputError, setInputError] = useState<string | null>(null);
+
   const [underlineOnFocus, setUnderlineOnFocus] = useState(false);
 
-  const toggleFocus = () => setUnderlineOnFocus((prev) => !prev);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value;
+      setInternalValue(newValue);
+      setInputError(null); // Clear error on input change
+    };
+  
+  const handleFocus = () => {
+    setIsFocused(true);
+    setUnderlineOnFocus(true);
+    setInputError(null); // Clear error on focus
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    setUnderlineOnFocus(false);
+    if (internalValue !== "") {
+      setInternalValue(internalValue); // Format and update input with formatted date
+      onChange(internalValue); // Call onChange with the formatted date
+    } else {
+      setInputError('Please input a valid value.');
+    }
+  };
 
   // Determine font size based on label presence
   const fontSize = label == null ? "24px" : "18px";
 
   return (
-    <div className={styles.textfieldcontainer}>
+    <div className={`${styles['textfieldcontainer']} ${isFocused && underlineOnFocus ? styles['underline'] : ''}`}>
       {label && (
         <label
           className={styles.textfieldlabel}
@@ -36,15 +62,20 @@ const TextField: React.FC<TextFieldProps> = ({
       )}
       <input
         type="text"
-        value={value}
-        onFocus={toggleFocus}
-        onBlur={toggleFocus}
-        onChange={(e) => onChange(e.target.value)}
-        className={`${styles.textfieldinput} ${underlineOnFocus ? styles.focused : styles.default}`}
+        value={internalValue}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         placeholder={input}
         style={{ fontSize }}
+        className={`${styles.textfieldinput} ${inputError ? styles['error-input'] : ''}`} // Apply error class conditionally
         {...props}
       />
+      {inputError && (
+        <div className={styles['error-message']}>
+          {inputError}
+        </div>
+      )}
     </div>
   );
 };
