25d487d535afa294fb5006d9d2fc0aebafb41b5b
Tuni Le <ltn090403@gmail.com>
2025-04-22 23:47:33 -0400

    Refactor ViewMeeting, add display field for email in ViewMeeting

--- frontend/app/components/organisms/ViewMeeting.tsx
+++ frontend/app/components/organisms/ViewMeeting.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import styles from '../../../../styles/ViewMeeting.module.scss';
+import styles from '../../../styles/ViewMeeting.module.scss';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
-import { convertUTCToET } from "../../../../util/timeUtils";
+import { convertUTCToET } from "../../../util/timeUtils";
 
 type ViewMeetingDetailsProps = {
   mid: string; // Maps to 'mid' in the model
@@ -86,24 +86,13 @@ const ViewMeetingDetails: React.FC<ViewMeetingDetailsProps> = ({
         {recurrence && <p>{recurrence}</p>}
         <hr className={styles.divider} />
 
+        <p><strong>Email:</strong>&nbsp;{email}</p>
         <p><strong>Calendar:</strong>&nbsp;{group}</p>
         <p><strong>Location:</strong>&nbsp;{room}</p>
         {zoomAccount && <p><strong>Zoom Account:</strong>&nbsp;{zoomAccount}</p>}
         {zoomLink && <a href={zoomLink} target="_blank" rel="noopener noreferrer" className={styles.zoomLink}>
           <VideoCameraFrontIcon /> {zoomLink}
         </a>}
-        <p><strong>PandaDocs Form</strong></p>
-        <div className={styles.pandaDocs}>
-
-          <a href={'https://Google.com'} download className={styles.pandaDocsLink}>
-            <div className={styles.docsEmoji}><InsertDriveFileIcon /></div>
-            <div className={styles.pandaDocsText}>
-              <div className={styles.pandaDocsName}>{"Dummy Name"}</div>
-              <div className={styles.pandaDocsSize}>{"1.2 MB"}</div>
-            </div>
-            <div className={styles.downloadEmoji}><DownloadForOfflineIcon /></div>
-          </a>
-        </div>
 
         <hr className={styles.divider} />
 
